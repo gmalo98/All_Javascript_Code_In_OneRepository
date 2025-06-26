04e8d0b5a67d3f18bfb45ef41ac14b2f4db344ad
Ganesh Malo <malo.ganesh98@gmail.com>
2025-06-26 21:30:37 +0530

    Leetcode RemoveElemet solution

--- PrintUniqueElement.js
+++ PrintUniqueElement.js
@@ -26,3 +26,14 @@ if(arr.indexOf[arr[i]===arr.lastIndexOf(arr[i])])
     console.log(arr[i]);
 }
 
+const val=2;
+const nums=[0,1,2,2,3,0,4,2];
+var removeElement = function(nums, val) {
+    for(let i=0;i<nums.length;i++){
+      if(nums[i]===val)
+      nums.splice(i,1);  
+    }
+    return nums;
+};
+const k=removeElement(nums,val)
+console.log(k);
